9ee0329f4cc8f0d7903cb3a1f5ab9bfb1b50fe31
zeinrady <radyzein2003@gmail.com>
2025-09-18 17:17:27 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+  server:
+    {
     host: "::",
     port: 8080,
   },
+    base: '/portfolio-site/',
+
   // Removed lovable-tagger plugin from the Vite configuration
   plugins: [react()],
   resolve: {
